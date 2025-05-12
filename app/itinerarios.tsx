954338c3c6ea78c369e7fd0b7a6d4183f99c579a
mlc1503 <manelagunas1503@gmail.com>
2025-05-13 01:43:00 +0200

    almost!

--- app/itinerarios.tsx
+++ app/itinerarios.tsx
@@ -36,68 +36,81 @@ export default function Index() {
 	const {origin, destination, line_id} = useLocalSearchParams();
 
 	const db = useSQLiteContext();
-	const [data, setData] = useState<RouteData[]>([]);
-	const [all_route_stations, setRouteStations] = useState<RouteStations[]>([]);
 
-	const [itinerary] = useState<ItineraryStation[]>([]);
-	const [labelLine, setLabelLine] = useState('');
+	const [itinerary, setItinerary] = useState<ItineraryStation[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [labelLine, setLabelLine] = useState('test');
 
 	let id:number = typeof line_id === 'string' ? parseInt(line_id) : parseInt(line_id[0])
 	
 	useEffect(() => {
 		
-		if(origin == null || destination == null){
-			db.withTransactionAsync(async () => {
-				await getFullRouteData(id)
-			})
-		}
-
+		loadData(id)
+		setIsLoading(false)
+		
 	}, [db])
 
-	async function getFullRouteData(id:number) {
+	const loadData = async (id:number)=> {
 		
-		db.getAllAsync<RouteData>(`
-			SELECT l.id as line_id, l.line_name AS line_name, s.stop_id, s.name AS stop_name
-			FROM stops s 
-				JOIN route_stations r ON s.stop_id = r.stop_id 
-				JOIN lines l ON r.route_id = l.id
-			WHERE l.id = ?;
-		`, [id])
-		.then((result) =>{
-			setData(result)
-		})
-		.catch((err)=>{
-			console.error("GET ITINERARIO FAILED", err);
-		})
+		const route_data = db.getAllAsync<RouteData>(`
+								SELECT l.id as line_id, l.line_name AS line_name, s.stop_id, s.name AS stop_name
+								FROM stops s 
+									JOIN route_stations r ON s.stop_id = r.stop_id 
+									JOIN lines l ON r.route_id = l.id
+								WHERE l.id = ?;
+							`, [id])
 
-		db.getAllAsync<RouteStations>(`
-			SELECT r.stop_id, l.id as line_id 
-			FROM route_stations r 
-				JOIN lines l ON l.id = r.route_id;
-		`)
-		.then((result)=> setRouteStations(result))
-		.catch((err)=>console.error("GET ROUTE_STATIONS ITINERARIO FAILED", err))	
-	}
+		const all_route_stations = db.getAllAsync<RouteStations>(`
+										SELECT r.stop_id, l.id as line_id 
+										FROM route_stations r 
+											JOIN lines l ON l.id = r.route_id;
+									`)
+		
 
+		let itinerary:ItineraryStation[] = [];
+		
 
-	if(itinerary.length == 0){
+		(await route_data).forEach(async estacion =>{
 
-		data.forEach(estacion => {
-			let route_lines_array_by_station:number[] = [];
-			let entries = all_route_stations.filter((rs)=> rs.stop_id == estacion.stop_id)
+			let line_correspondences_of_station:number[] = [];
+
+			//finds ocurrences of station_id in all the route_stations
+			let entries = (await all_route_stations).filter((rs)=> rs.stop_id == estacion.stop_id)
 	
 			entries.forEach(entry => {
-				route_lines_array_by_station.push(entry.line_id)
+				line_correspondences_of_station.push(entry.line_id)
 			});
 	
-			itinerary.push(new ItineraryStation(estacion.stop_id, estacion.stop_name, route_lines_array_by_station));
+			itinerary.push(new ItineraryStation(estacion.stop_id, estacion.stop_name, line_correspondences_of_station));
 			
-		}, [data]);
-		
+		}, [])
+		setItinerary(itinerary)
 	}
-	console.log("ITINERARIO COMPLETO\n", itinerary);
+
+	const label = async () => {
+		if(labelLine == 'test'){
+			// setLabelLine(itinerary[0].name + ' - ' + itinerary[itinerary.length	-1].name);
+		}
+	}
+
+	label()
+
+	if(isLoading){
+		return(
+			<View style={{
+				margin: constants.bounds.padding,
+				flexDirection: 'column',
+				rowGap: constants.bounds.padding,
+				alignContent: "center",
+				alignItems: "center",
+			}}>
+				<Text>FETCHEANDO DATOS...</Text>
+			</View>
+		)
+	}
 
 
+	
 	return (
 		<View style={{
 			margin: constants.bounds.padding,
@@ -108,6 +121,8 @@ export default function Index() {
 		}}>
 
 			{getArrayIconoLineas([id], 50)}
+
+			<Text>{labelLine}</Text>
 
 			{itinerary.map((item, index) =>{
 				return(
