14fab34448b4a9c36815c3dfd5fa726d255fc7c8
mlc1503 <manelagunas1503@gmail.com>
2025-05-18 01:17:05 +0200

    la DB no se descargaba. que susto coño

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack} from "expo-router";
-import { StatusBar, useColorScheme, View } from "react-native";
+import { ActivityIndicator, StatusBar, useColorScheme, View } from "react-native";
 import { useFonts } from 'expo-font';
 import { useEffect, useState } from "react";
 import { colorsList, IconoCTFV } from "./constants/Constants";
@@ -13,50 +13,89 @@ import * as FileSystem from 'expo-file-system'
 
 SplashScreen.preventAutoHideAsync()
 
+const dbName = "metro_soria.db";
+
 const loadDatabase = async()=>{
-	const dbName = "metro_soria.db";
 	const dbAsset = require('@/assets/db/metro_soria.db');
 	const dbUri = Asset.fromModule(dbAsset).uri;
 	const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`
 	
 	const fileInfo = await FileSystem.getInfoAsync(dbFilePath)
 	
+	//comprobamos si directorio existe
 	if(!fileInfo.exists){
-		await FileSystem.makeDirectoryAsync(
-			`${FileSystem.documentDirectory}SQLite`, 
-			{intermediates: true}
-		);
+		await FileSystem.makeDirectoryAsync(`${FileSystem.documentDirectory}SQLite`, {intermediates: true});
 	}
-	await FileSystem.downloadAsync(dbUri, dbFilePath)
-	console.log("se ha descargado el fichero .db al dispositivo");
+
+	//borramos DB si existe
+	try {
+		await FileSystem.deleteAsync(dbFilePath);
+		console.log("Old DB file deleted");
+		
+	} catch (error) {
+		console.error("No hay archivo DB que borrar");
+	}
+
+
+	//descargamos DB nueva
+	try {
+		await FileSystem.downloadAsync(dbUri, dbFilePath)
+		console.log("Nuevo archivo DB descargado");
+
+		//verificamos descarga de DB
+		const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+		if(!fileInfo.exists){
+			throw new Error("La descarga de la DB no es válida");
+		}
+
+		console.log("La DB está lista en:", dbFilePath);
+	} catch (error) {
+		console.error("Failed to download DB", error);
+		throw error;
+		
+	}
+	console.log();
+	
 }
 
 export default function RootLayout() {
 
 	const colorScheme = useColorScheme();
-	const [loaded] = useFonts({
+	const [fontsLoaded] = useFonts({
 		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
 	});
 
 	const [dbLoaded, setDbLoaded] = useState(false)
 
-	
-
 	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync();
+
+
+		const setApp = async() =>{
+			try {
+				await loadDatabase()
+				setDbLoaded(true)
+				
+			} catch (error) {
+				throw error;
+			}
+			finally{
+				SplashScreen.hideAsync()
+			}
 		}
 
-		loadDatabase()
-			.then(()=> {
-				setDbLoaded(true); 
-				console.log("se ha cargado la bd");
-			})
-			.catch((error) => console.error("Un error ha ocurrido al cargar la DB.\n",error))
-	}, [loaded, dbLoaded]);
+		if(fontsLoaded){
+			setApp()
+		}
 
-	if (!loaded) {
-		return null;
+	}, [fontsLoaded]);
+
+
+	if(!fontsLoaded || !dbLoaded){
+		return(
+			<View style={{flex: 1, justifyContent: "center", alignItems:"center"}}>
+				<ActivityIndicator size="large" color={colorsList.light.ALERT_RED} />
+			</View>
+		)
 	}
 
 
@@ -67,7 +106,7 @@ export default function RootLayout() {
 			// 	<View style={{flex: 1, backgroundColor: colorsList.light.ALERT_RED}}/>
 			// }
 		>
-			<SQLiteProvider databaseName="test.db" useSuspense>
+			<SQLiteProvider databaseName="metro_soria.db" useSuspense>
 				<StatusBar barStyle="default" backgroundColor={colorScheme === 'dark' ? colorsList.light.MAIN_BLACK : colorsList.light.PRIMARY_BLUE} />
 				<Stack screenOptions={{
 					headerTitle: ()=> IconoCTFV,
